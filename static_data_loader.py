47a84a3513c75038b5b59d3b13e89541e4bfcf35
ZhivkoTsvetanov <83118420+ZhivkoTsvetanov@users.noreply.github.com>
2022-10-03 12:19:44 +0300

    Update static_data_loader.py

--- static_data_loader.py
+++ static_data_loader.py
@@ -10,10 +10,6 @@ class StaticDataLoader:
                             format='[%(levelname)-8s %(asctime)s] %(message)s')
         self.logger = logging.getLogger(__name__)
 
-    def load_label_tags_config(self):
-        with open('label_tags_config.yaml', 'r') as ymlfile:
-            return yaml.safe_load(ymlfile)
-
     def load_static_data(self, context):
         try:
             with open(os.path.join('test_data', 'static_data.yaml'), 'r') as ymlfile:
@@ -21,4 +17,4 @@ class StaticDataLoader:
 
         except IOError:
             self.logger.error(
-                'Could not find one of these files: ui_elements_vault.yaml, static_data.yaml, register_member.json, os_devices_list_members.yaml!')
+                'Something went wrong')
